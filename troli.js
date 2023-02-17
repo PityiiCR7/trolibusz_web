84333767a156da38c9140d1a0d566c2258f41ebd
PityiiCR7 <szoke.istvan07@gmail.com>
2023-02-17 13:15:12 +0100

    Feladat 9

--- troli.js
+++ troli.js
@@ -22,7 +22,7 @@ function jaratKiiras(elem, index){
 }
 
 function vegallomasKiiras(jarat){
-    document.getElementById("vegallamasok").innerHTML=vegallomasNevek[jarat];
+    document.getElementById("vegallomasok").innerHTML=vegallomasNevek[jarat];
 }
 
 function fel() {
